7985eee1144f8edeb8462a2cac0c716e68aee816
linqh1 <qihua.lin@quantil.com>
2019-03-22 16:36:55 +0800

    增加处理ICMP报文的逻辑

--- entry/udp-client.go
+++ entry/udp-client.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
+	"golang.org/x/net/icmp"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -15,11 +18,17 @@ func checkError(err error) {
 }
 
 func main() {
-	serverAddr := "10.8.156.137:10006"
+	serverAddr := "10.8.227.27:10006"
 	conn, err := net.Dial("udp", serverAddr)
 	checkError(err)
-
 	defer conn.Close()
+
+	ipport := conn.LocalAddr().String()
+	packetConn, err := icmp.ListenPacket("ip4:icmp", ipport[:strings.LastIndex(ipport, ":")])
+	if err != nil {
+		panic("ListenPacket Error:" + err.Error())
+	}
+	defer packetConn.Close()
 
 	bytes := []byte("i'm from vmware")
 	n, err := conn.Write(bytes)
@@ -28,6 +37,30 @@ func main() {
 	msg := make([]byte, 100)
 	conn.SetReadDeadline(time.Now().Add(time.Second * 5))
 	n, err = conn.Read(msg)
-	checkError(err)
-	fmt.Println("Response:", string(msg[:n]))
+	if err != nil {
+		icmpmsg := make([]byte, 1024)
+		packetConn.SetReadDeadline(time.Now().Add(time.Second))
+		// 注意：ICMP响应报文通常情况下会被拦截
+		i, addr, err := packetConn.ReadFrom(icmpmsg)
+		if err != nil {
+			fmt.Printf("ReadFrom UDP ERROR:%v\n", err.Error())
+		} else {
+			fmt.Println("received icmp message")
+			fmt.Println(addr.String())
+			fmt.Print(hex.Dump(icmpmsg[:i]))
+			hanlderICMPPacked(icmpmsg[:i])
+		}
+	} else {
+		fmt.Println("Response:", string(msg[:n]))
+	}
+}
+
+// 处理icmp报文
+func hanlderICMPPacked(bytes []byte) {
+	itype := bytes[0]
+	icode := bytes[1]
+	ichecksum := int(bytes[2])*256 + int(bytes[3])
+	fmt.Printf("icmp type:%v\n", itype)
+	fmt.Printf("icmp code:%v\n", icode)
+	fmt.Printf("icmp checksum:%v\n", ichecksum)
 }
