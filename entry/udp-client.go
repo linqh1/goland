e7da6ce6e9a5ed6ae921e9240f7cfe46eb546b9a
linqh1 <qihua.lin@quantil.com>
2019-03-22 14:09:35 +0800

    修改代码：直接发送字符串，而不是读取用户输入字符串

--- entry/udp-client.go
+++ entry/udp-client.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 func checkError(err error) {
@@ -15,37 +15,19 @@ func checkError(err error) {
 }
 
 func main() {
-	serverAddr := "127.0.0.1:10006"
+	serverAddr := "10.8.156.137:10006"
 	conn, err := net.Dial("udp", serverAddr)
 	checkError(err)
 
 	defer conn.Close()
 
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		line := input.Text()
-
-		lineLen := len(line)
-
-		n := 0
-		for written := 0; written < lineLen; written += n {
-			var toWrite string
-			if lineLen-written > 10 {
-				toWrite = line[written : written+10]
-			} else {
-				toWrite = line[written:]
-			}
-
-			n, err = conn.Write([]byte(toWrite))
-			checkError(err)
-
-			fmt.Println("Write:", toWrite)
-
-			msg := make([]byte, 10)
-			n, err = conn.Read(msg)
-			checkError(err)
-
-			fmt.Println("Response:", string(msg))
-		}
-	}
+	bytes := []byte("i'm from vmware")
+	n, err := conn.Write(bytes)
+	checkError(err)
+	fmt.Println("Write:", string(bytes[:n]))
+	msg := make([]byte, 100)
+	conn.SetReadDeadline(time.Now().Add(time.Second * 5))
+	n, err = conn.Read(msg)
+	checkError(err)
+	fmt.Println("Response:", string(msg[:n]))
 }
